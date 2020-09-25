f5f3cb8eef589f75999afb9d82b1279d03402bc0
maximus1127 <maxbourque1127@yahoo.com>
2020-09-25 12:00:48 -0500

    added lensometer modal

--- resources/views/modals/modals.blade.php
+++ resources/views/modals/modals.blade.php
@@ -61,3 +61,34 @@
   </div>
 </div>
 {{-- end auto refractor modal --}}
+
+
+{{-- auto lensometer modal --}}
+<div class="modal fade" id="autolensometerModal" tabindex="-1" role="dialog" aria-labelledby="autolensometerModal" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="autolensometerModalLabel">Autolensometer</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <table style="width: 100%">
+          <thead>
+            <th>Timestamp</th>
+            <th>Readings</th>
+          </thead>
+          <tbody id="autolensometerBody">
+
+          </tbody>
+        </table>
+      </div>
+      {{-- <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div> --}}
+    </div>
+  </div>
+</div>
+{{-- end auto lensometer modal --}}
